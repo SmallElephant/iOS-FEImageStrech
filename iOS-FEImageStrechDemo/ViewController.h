86d3cafdf6e9d468bf6b2f4e024778a7cfd241cb
FlyElephant <1061731917@qq.com>
2016-01-13 15:38:51 +0800

    Initial Commit

---
+++ iOS-FEImageStrechDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS-FEImageStrechDemo
+//
+//  Created by keso on 16/1/13.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
